56f7f1d65e009762a9bdcd9f0f36939c34cd42ab
huibunny <cc@huibunny.com>
2022-08-23 21:06:54 +0800

    +duboo

--- down.sh
+++ down.sh
@@ -3,10 +3,10 @@
 # -p specify project name.It separates each docker-compose file.
 docker-compose -p consul        -f docker-compose-consul-cluster.yml    down
 docker-compose -p postgres      -f docker-compose-postgres.yml          down
-#docker-compose -p mysql         -f docker-compose-mysql.yml             down
+docker-compose -p mysql         -f docker-compose-mysql.yml             down
 docker-compose -p redis         -f docker-compose-redis.yml             down
 docker-compose -p rabbitmq      -f docker-compose-rabbitmq.yml          down
-docker-compose -p nacos         -f docker-compose-nacos-m1.yml          down
+#docker-compose -p nacos         -f docker-compose-nacos-m1.yml          down
 docker-compose -p minio         -f docker-compose-minio.yml             down
 docker-compose -p jenkins       -f docker-compose-jenkins.yml           down
-docker-compose -p zookeeper     -f docker-compose-zookeeper.yml         down
+docker-compose -p dubbo         -f docker-compose-dubbo.yml             down
--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # -p specify project name.It separates each docker-compose file.
 docker-compose -p consul       -f docker-compose-consul-cluster.yml   up -d
 docker-compose -p postgres     -f docker-compose-postgres.yml         up -d
-#docker-compose -p mysql        -f docker-compose-mysql.yml            up -d
+docker-compose -p mysql        -f docker-compose-mysql.yml            up -d
 docker-compose -p redis        -f docker-compose-redis.yml            up -d
 docker-compose -p rabbitmq     -f docker-compose-rabbitmq.yml         up -d
-docker-compose -p nacos        -f docker-compose-nacos-m1.yml         up -d
+#docker-compose -p nacos        -f docker-compose-nacos-m1.yml         up -d
 docker-compose -p minio        -f docker-compose-minio.yml            up -d
 docker-compose -p jenkins      -f docker-compose-jenkins.yml          up -d
-docker-compose -p zookeeper    -f docker-compose-zookeeper.yml        up -d
+docker-compose -p dubbo        -f docker-compose-dubbo.yml            up -d
