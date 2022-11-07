54b474449a2718a3f92529aef8ddbb529f2c0b0e
huibunny <cc@hubunny.com>
2022-11-07 14:48:07 +0800

    +install.sh

--- down.sh
+++ down.sh
@@ -2,7 +2,7 @@
 # ![docker compose orphan containers warning](https://stackoverflow.com/questions/50947938/docker-compose-orphan-containers-warning)
 # -p specify project name.It separates each docker-compose file.
 #docker-compose -p consul        -f docker-compose-consul-cluster.yml    down
-docker-compose -p postgres      -f docker-compose-postgres.yml          down
+#docker-compose -p postgres      -f docker-compose-postgres.yml          down
 #docker-compose -p mysql         -f docker-compose-mysql.yml             down
 # mysql5
 #docker-compose -p mysql57       -f docker-compose-mysql5.7.yml          down
@@ -15,4 +15,4 @@ docker-compose -p postgres      -f docker-compose-postgres.yml          down
 #docker-compose -p dubbo         -f docker-compose-dubbo.yml             down
 #docker-compose -p nginx         -f docker-compose-nginx.yml             down
 #docker-compose -p fs            -f docker-compose-fs.yml                down
-docker-compose -p flowable      -f docker-compose-flowable.yml          down
+#docker-compose -p flowable      -f docker-compose-flowable.yml          down
---
+++ script/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#3.进入apt-get 配置目录
+cd /ect/apt
+#4.执行备份命令 --避免修改失败无法使用
+cp sources.list sources.list.bak
+
+#5.同时执行echo下的4行命令，修改成国内镜像源
+echo "">sources.list
+echo "deb http://ftp2.cn.debian.org/debian/ buster main">>sources.list
+echo "deb http://ftp2.cn.debian.org/debian/debian-security buster/updates main">>sources.list
+echo "deb http://ftp2.cn.debian.org/debian/debian buster-updates main">>sources.list
+
+apt update
+apt install -y vim telnet
